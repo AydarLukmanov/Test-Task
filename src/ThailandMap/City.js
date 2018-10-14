28145eb68df55cc34dbefb4b8581feb08217a240
aydarluk <aydarluk@gmail.com>
2018-10-14 15:14:32 +0300

    google maps

---
+++ src/ThailandMap/City.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const City = ({ name }) => <div className="city">{name}</div>;
+
+export default City;
---
+++ src/ThailandMap/index.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+import City from './City';
+
+const citiesMap = {
+  A: {
+    name: 'Bangkok',
+    lat: 13.7563,
+    lng: 100.5018,
+  },
+  B: {
+    name: 'Pattaya',
+    lat: 12.9236,
+    lng: 100.8825,
+  },
+  C: {
+    name: 'Chiang Mai',
+    lat: 18.7061,
+    lng: 98.9817,
+  },
+  D: {
+    name: 'Phitsanulok',
+    lat: 16.8211,
+    lng: 100.2659,
+  },
+  E: {
+    name: 'Khon Kaen',
+    lat: 16.4322,
+    lng: 102.8236,
+  },
+  F: {
+    name: 'Chumphon Province',
+    lat: 10.4930,
+    lng: 99.1800,
+  },
+};
+
+const exampleWeights = 'AB1, AC4, AD10, BE3, CD4, CF2, DE1, EB3, EA2, FD1';
+
+const grapth = exampleWeights.split(', ').map(str => {
+  const from = citiesMap[str[0]];
+  const to = citiesMap[str[1]];
+  const weight = parseInt(str[2]);
+  return {
+    from,
+    to,
+    weight,
+  };
+});
+
+export default class ThailandMap extends Component {
+  static defaultProps = {
+    center: {
+      lat: 14.7563,
+      lng: 100.5018,
+    },
+    zoom: 7
+  };
+
+  render() {
+    return (
+      <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyAtN7-KIU2yT68bkrgBXRyIxLL_blLGf4M' }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          {
+            Object.keys(citiesMap).map(key => {
+              const { name, lat, lng } = citiesMap[key];
+              return (
+                <City
+                  key={key}
+                  lat={lat}
+                  lng={lng}
+                  name={name}
+                />
+              );
+            })
+          }
+        </GoogleMapReact>
+      </div>
+    );
+  }
+}
